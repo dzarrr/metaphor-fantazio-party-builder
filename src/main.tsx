6006e17a09fb6d1fee4a0730fcf2bf2ecd493296
dzarrr <dzarbelahanifa@gmail.com>
2024-10-09 14:29:42 +0700

    feat: initialize joy mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@fontsource/inter';
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@fontsource/inter";
+import { CssVarsProvider } from "@mui/joy/styles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CssVarsProvider>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
+  </StrictMode>
+);
