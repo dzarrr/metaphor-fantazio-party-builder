5cedd27353766a75ccd8a2132b94bc1ed4b1c015
dzarrr <dzarbelahanifa@gmail.com>
2024-10-09 15:51:44 +0700

    chore: remove unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import { CssVarsProvider } from "@mui/joy/styles";
-import { Sheet } from "@mui/joy";
 import CssBaseline from "@mui/joy/CssBaseline";
-import Typography from "@mui/joy/Typography";
-import Select from "@mui/joy/Select";
-import Option from "@mui/joy/Option";
 import { ArchetypeCard } from "./components/ArchetypeCard";
-import { archetypeList } from "./const/archetypeList";
-
 import "./App.css";
 
 function App() {
